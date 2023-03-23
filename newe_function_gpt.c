d3f1948ce1c67c6640c8f786334aab40ac297558
atucci <alessiotucci33@gmail.com>
2023-03-23 15:16:51 +0100

    added the all in one function

--- newe_function_gpt.c
+++ newe_function_gpt.c
@@ -1,29 +1,15 @@
-/**
- * check_flags - function to check both sign flags and formatting flags in a format string
- * @str: pointer to the format string
- * @i: index of the character in the format string
- *
- * Return: the index of the last character before the conversion specifier
- */
 int check_flags(const char *str, int i)
 {
     int index = i + 1;
-
-    // Check if the first character is '0'
     if (str[i] == '0')
     {
         g_bonus.is_zero = 1;
         index++;
     }
-
-    // Loop through the string to find flags
     while (str[index])
     {
-        // If a conversion specifier is found, exit the loop
         if (ft_strchr("cspdiuxX%", str[index]))
             break;
-
-        // Check for sign flags
         if (str[index] == '+')
         {
             g_bonus.is_plus = 1;
@@ -39,8 +25,6 @@ int check_flags(const char *str, int i)
             g_bonus.is_minus = 2;
             index++;
         }
-
-        // Check for formatting flags
         if (ft_isdigit(str[index]))
         {
             if (!has_precision && !has_width)
@@ -68,12 +52,7 @@ int check_flags(const char *str, int i)
                 g_bonus.precision = 0;
             }
         }
-
-        // Move to the next character
         index++;
     }
-
-    // Return the index of the last character before the conversion specifier
-    return index - 1;
+    return (index - 1);
 }
-
